1e34ce9fec4c8b6e6f05d69dcb5ed95e797dff31
Ogliss <randomdragon2@hotmail.com>
2020-07-20 14:35:50 +0100

    inital commit

---
+++ Source/ModOptions/PGISettings.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace PerfectlyGenericItem
+{
+    public class PGISettings : ModSettings
+    {
+        public bool removePGI = false;
+        public bool replaceFactions = false;
+
+        public PGISettings()
+        {
+            PGISettings.Instance = this;
+        }
+
+        public static PGISettings Instance;
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref this.removePGI, "PGI_remove", false);
+            Scribe_Values.Look(ref this.replaceFactions, "PGI_replaceFactions", false);
+
+        }
+    }
+}
