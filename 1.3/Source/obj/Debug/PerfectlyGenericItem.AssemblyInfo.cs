ef782f7b5f5c01956a27c79fefeb2bff65fa03dd
Ogliss <randomdragon2@hotmail.com>
2022-10-04 07:46:11 +0100

    com trouble

--- 1.3/Source/obj/Debug/PerfectlyGenericItem.AssemblyInfo.cs
+++ 1.3/Source/obj/Debug/PerfectlyGenericItem.AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PerfectlyGenericItem")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright �  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
