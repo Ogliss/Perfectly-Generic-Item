d17aaec895561de33749afdd7242e7bf6e26c5ae
Ogliss <randomdragon2@hotmail.com>
2022-10-24 18:53:27 +0100

    inital 1.4 commit

---
+++ 1.4/Source/Harmony/PGI_GenStuff_CanMake_Patch.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using RimWorld;
+using RimWorld.BaseGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace PerfectlyGenericItem 
+{
+    [HarmonyPatch(typeof(StuffProperties), "CanMake")]
+    public static class PGI_GenStuff_CanMake_Patch
+    {
+        [HarmonyPostfix, HarmonyPriority(Priority.Last)]
+        public static void Postfix(StuffProperties __instance, ref bool __result)
+        {
+            if (__instance.parent == null)
+            {
+                return;
+            }
+            if (__instance.parent == PGIThingDefOf.PerfectlyGenericItem)
+            {
+                __result = false;
+            }
+        }
+    }
+
+}
